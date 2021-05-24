f02642ca4120fb1d9d66499ee85ac7c4114d0ae7
Thijs Busser <tbusser@gmail.com>
2021-05-24 19:59:28 +0200

    Add comments to RoundStatus enum

--- src/scripts/01-global/enum/RoundStatus.ts
+++ src/scripts/01-global/enum/RoundStatus.ts
@@ -1,5 +1,16 @@
 export enum RoundStatus {
+	/**
+	 * The round is still being played.
+	 */
 	InProgress = 'in-progress',
+
+	/**
+	 * All players have been unable to play a tile, the round is locked.
+	 */
 	Locked = 'locked',
+
+	/**
+	 * There is a player with an empty hand, this player has won the round.
+	 */
 	Won = 'won'
 }
